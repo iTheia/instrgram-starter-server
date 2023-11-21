c89fb39fb1c7f6699b24503e3c923e7484387bc0
Sirakusa <herrera.clip@gmail.com>
2023-11-20 21:47:26 -0600

    add paths of follow module

--- app/router.ts
+++ app/router.ts
@@ -3,12 +3,14 @@ import { healthCheckRouter } from "./modules";
 
 import { routerAuth } from "./modules/Auth/router";
 import { routerPost } from "./modules/Post/router";
+import { routerFollow } from "./modules/Follow/router";
 
 export default function (app: Express) {
   app.use("/health-check", healthCheckRouter);
 
   app.use("/auth", routerAuth);
   app.use("/post", routerPost);
+  app.use("/follow", routerFollow);
 
   app.use((_, res) => {
     res.status(404).json({ error: "Not Found" });
